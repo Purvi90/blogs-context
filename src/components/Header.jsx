99cd8d54dcd769f25ca3444c4ccd0a90f0510bf9
Purvi90 <purvisharma1239@gmail.com>
2025-01-31 22:03:36 +0530

    changes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <div className=' w-full border shadow-md py-2'>
+    <div className=' w-full border shadow-md py-4'>
         <header className='text-center'>
           <h1 className='text-xl font-bold uppercase' >Blogs Context</h1>
         </header>
